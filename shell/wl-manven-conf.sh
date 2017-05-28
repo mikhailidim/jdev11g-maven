b7a02922329332ac7e12ce1d4358d47c232b2d77
mikhailidim <mikhailidim@gmail.com>
2017-05-28 15:12:25 -0400

    Update Linux shell scripts.

--- shell/wl-manven-conf.sh
+++ shell/wl-manven-conf.sh
@@ -20,7 +20,7 @@ echo MVN_HOME=$MVN_HOME |tee -a $LOG
 echo TEMP=$TMP/wlmaven
 echo -e "\n-------------------------------------"
 $JAVA_HOME/bin/java -version |tee -a $LOG
-$MVN_HOME/bin/mvn -version |tee -a $LOG
+$MVN_HOME/bin/mvn6 -version |tee -a $LOG
 
 
 echo -e "\nPrepare plugin" |tee -a $LOG
@@ -31,6 +31,7 @@ mkdir $TMP/wlmaven
 cp wls-pom.xml $TMP/wlmaven/pom.xml
 
 cd $WLS_HOME/server/lib/
+$JAVA_HOME/bin/java -jar $WLS_HOME/server/lib/wljarbuilder.jar -profile wlfullclient |tee -a $LOG
 $JAVA_HOME/bin/java -jar $WLS_HOME/server/lib/wljarbuilder.jar -profile weblogic-maven-plugin |tee -a $LOG
 mv weblogic-maven-plugin.jar $TMP/wlmaven
 
@@ -38,17 +39,17 @@ cd $TMP/wlmaven
 
 
 echo -e "\nInstall Weblogic Related libraries" |tee -a >> $LOG
-$MVN_HOME/bin/mvn install:install-file -DgroupId=com.oracle.cryptoj -DartifactId=cryptoj -Dversion=1.0 -Dpackaging=jar -Dfile=$FMW_HOME/modules/cryptoj.jar -DgeneratePom=true |tee -a $LOG
+$MVN_HOME/bin/mvn6 install:install-file -DgroupId=com.oracle.cryptoj -DartifactId=cryptoj -Dversion=1.0 -Dpackaging=jar -Dfile=$FMW_HOME/modules/cryptoj.jar -DgeneratePom=true |tee -a $LOG
 $MVN_HOME/bin/mvn install:install-file -DgroupId=weblogic -DartifactId=weblogic -Dversion=10.3.6 -Dpackaging=jar -Dfile=$WLS_HOME/server/lib/weblogic.jar  |tee -a $LOG
 $MVN_HOME/bin/mvn install:install-file -DgroupId=weblogic -DartifactId=weblogic -Dversion=10.3.6  -Dpackaging=jar -Dfile=$WLS_HOME/server/lib/wlfullclient.jar  |tee -a $LOG
 $MVN_HOME/bin/mvn install:install-file -DgroupId=weblogic -DartifactId=webservices -Dversion=10.3.6  -Dpackaging=jar -Dfile=$WLS_HOME/server/lib/webservices.jar  |tee -a $LOG
 
 echo -e "\nInstall SOA Runtime helpers" |tee -a $LOG
-call $MVN_HOME/bin/mvn install:install-file -DgroupId=oracle.soa.utils -DartifactId=common -Dversion=11.1  -Dpackaging=jar -Dfile=$ORACLE_HOME/soa/modules/oracle.soa.fabric_11.1.1/fabric-runtime.jar  -DgeneratePom=true |tee -a $LOG
+$MVN_HOME/bin/mvn6 install:install-file -DgroupId=oracle.soa.utils -DartifactId=common -Dversion=11.1  -Dpackaging=jar -Dfile=$ORACLE_HOME/soa/modules/oracle.soa.fabric_11.1.1/fabric-runtime.jar  -DgeneratePom=true |tee -a $LOG
 
 
 echo -e "\nInstall maven plugin"  |tee -a $LOG
-$MVN_HOME/bin/mvn install:install-file -Dfile=weblogic-maven-plugin.jar -DpomFile=pom.xml  |tee -a $LOG
+$MVN_HOME/bin/mvn6 install:install-file -Dfile=weblogic-maven-plugin.jar -DpomFile=pom.xml  |tee -a $LOG
 
 popd 
 
@@ -57,5 +58,5 @@ rm -rf $TMP/wlmaven
 
 #Validate configuration 
 echo -e "\nTest Weblogic plugin"  |tee -a $LOG
-$MVN_HOME/bin/mvn  weblogic:help |tee -a $LOG
+$MVN_HOME/bin/mvn6  weblogic:help |tee -a $LOG
 
